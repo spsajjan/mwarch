e50787f72a53f22dac7bb6e0d53de7057d1f49d9
spsajjan <spsajjan7016@gmail.com>
2025-01-21 13:27:00 +0530

    major update 1

--- install.sh
+++ install.sh
@@ -24,17 +24,83 @@ echo """
                                   \____||__|__|  |__|  |__|__||___|___||____||__|__|  |__| 
 """
 echo
-echo "Starting pre-install..." && sleep 2
+echo "Starting pre-install..." && sleep 3
 
+# IMPORTANT VARIABLES
+ov="\033[0K\r" #To overwrite the same line
+
+
+#TESTING CODE
+
+
+
+
+# echo "Testing Finished" && exit
+############################################################################################
 # INSTALL PACKAGES
-declare -a PKGS=(kitty neofetch)
-for i in "${PKGS[@]}"; do
-    check=$(command -v "$i")
-    if [[ $? -ne 0 ]]; then
-        sudo apt install "$i"
+echo -ne "Installing Dependencies ....$ov" && sleep 1
+declare -a PKGS=(kitty neofetch python3 python3-pip)
+for REQUIRED_PKG in "${PKGS[@]}"; do
+  PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "package already installed")
+  echo Checking for $REQUIRED_PKG: $PKG_OK
+  if [ "" = "$PKG_OK" ]; then
+    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+    sudo apt-get --yes install $REQUIRED_PKG
+  fi
+done
+echo "Installed Necessary Dependencies" && sleep 1
+
+# ADDING WALLPAPERS
+echo -ne "Adding Wallpapers$ov" && sleep 1
+mkdir -p ~/Pictures/cm-wallie
+cp -R assets/wallpapers/. ~/Pictures/cm-wallie
+echo -e "Wallpapers Added" && sleep 2
+
+# CONFIGURE KITTY TERMINAL 
+echo -ne "Configuring Kitty Terminal$ov" && sleep 3
+echo -ne "Adding Kitty Terminal Background$ov" && sleep 1
+mkdir -p ~/.config/kitty/bg
+cp -R assets/terminal-bg/catamint-terminal-bg.png ~/.config/kitty/bg
+mv  ~/.config/kitty/kitty.conf ~/.config/kitty/kitty-original.conf
+cp -R app-config/kitty.conf ~/.config/kitty/
+echo "Kitty Terminal Configured" && sleep 2
+
+# CONFIGURE NEOFETCH 
+echo -ne "Configuring NeoFetch$ov" && sleep 2
+
+exists=$(grep -c "neofetch" ~/.bashrc)
+if [[ $exists -gt 0 ]]; then
+echo ""
+else
+echo 'neofecth' >> ~/.bashrc
+fi
+
+mkdir -p ~/.config/neofetch/png
+cp -R .meta/catamint-logo.png ~/.config/neofetch/png/
+mv  ~/.config/neofetch/config.conf ~/.config/neofetch/config-ori.conf
+cp -R app-config/neofetch-config.conf ~/.config/neofetch/
+mv  ~/.config/neofetch/neofetch-config.conf ~/.config/neofetch/config.conf
+
+echo "Neofetch Configured...."
+
+# INSTALL PYTHON DEPENDENCIES
+echo -en "Installing Python Dependencies... $ov" && sleep 2
+
+declare -a packages=(
+	"inquirer"
+	"loguru"
+	"psutil"
+	"gputil"
+	"pyamdgpuinfo"
+	"pyyaml"
+	"pillow"
+	"colorama"
+)
+
+for package in "${packages[@]}"; do
+    if ! pip show $package &> /dev/null; then
+        pip install $package --break-system-packages
     fi
 done
-echo "Installed Necessary Dependencies" && sleep 3
-echo "Adding Wallpapers..."
-mkdir ~/Pictures/cm-wallie
-cp -R wallpapers/. ~/Pictures/cm-wallie
+
+echo "Successfully Installed Python Dependencies"

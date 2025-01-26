a1ae6635f2fcac4cf2ce0be2ab99b5809ebedd47
spsajjan <spsajjan7016@gmail.com>
2025-01-26 16:20:33 +0530

    update

--- install.sh
+++ install.sh
@@ -1,43 +1,35 @@
 #!/bin/bash
 clear
 echo "Starting pre-install..." && sleep 3
-exit
 
 # UPGRADE THE SYSTEM
-echo "Updating and Updating the System Files..."
-sudo apt update
-sudo apt upgrade
+echo "Updating the System..."
+sudo pacman -Syu
 
 # INSTALL PYTHON AND ITS DEPENDENCIES
 echo "Installing Python Dependencies..." && sleep 2
-
-
-declare -a PKGS=(python3 python3-pip)
-for REQUIRED_PKG in "${PKGS[@]}"; do
-  PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "package already installed")
-  echo Checking for $REQUIRED_PKG: $PKG_OK
-  if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt-get --yes install $REQUIRED_PKG
-  fi
-done
+sudo pacman -Sy python-pip
 
 # Python Packages
 declare -a packages=(
-	"inquirer"
-	"loguru"
-	"psutil"
-	"gputil"
-	"pyamdgpuinfo"
-	"pyyaml"
-	"pillow"
-	"colorama"
+	"inquirer"      # Helps to ask question in shell prompt
+	"loguru"        # Logs colorful text with some decorations
+	"psutil"        # Process and System Utilities Information
+	"gputil"        # GPU Informations for NVIDIA
+	"pyamdgpuinfo"  # GPU Information for AMD GPU
+	"pyyaml"        # YAML parser and emitter
+	"pillow"        # File Support, Image Processing
+	"colorama"      # Adds color to text in the terminal
 )
-
 for package in "${packages[@]}"; do
     if ! pip show $package &> /dev/null; then
         pip install $package --break-system-packages
     fi
 done
+echo "Successfully Installed Python Dependencies" && sleep 2
 
-echo "Successfully Installed Python Dependencies"
+
+# INSTALLING PROCESS
+echo "Launching Installer" && sleep 2
+
+
